65c1bd5bd571b68314835b8cdadebb53c6f0a67e
이승학 <97331900+lshak123@users.noreply.github.com>
2023-02-27 22:29:40 +0900

    Add files via upload

---
+++ Python/Programmers/Lv.2 이진 변환 반복하기.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def solution(s):
+    answer = [0,0]
+    while True:
+        if s == '1':
+            break
+        answer[1] += s.count('0')
+        s = s.replace('0', '')
+        s = bin(len(s))[2:]
+        answer[0] += 1
+    return answer
+
